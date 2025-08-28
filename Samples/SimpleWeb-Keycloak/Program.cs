a743d612979582bd4a9260055ca344e6ec8e61c5
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2025-08-28 14:06:36 +0200

    add sample

---
+++ Samples/SimpleWeb-Keycloak/Program.cs
@@ -0,0 +1,33 @@
+using SimpleWeb_Keycloak;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+// SDK: add all services, read configuration etc. 
+builder.Services.AddDataPlaneSdk(builder.Configuration);
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+
+// Configure the HTTP request pipeline.
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+
+app.UseHttpsRedirection();
+
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();

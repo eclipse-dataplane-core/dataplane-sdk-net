4bf94941e515d2f8254b87a805fbbd0fac4115fb
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2025-10-17 13:32:18 +0200

    use async test assertions

--- Samples/Streaming/TestRunner/EndToEndTest.cs
+++ Samples/Streaming/TestRunner/EndToEndTest.cs
@@ -1,6 +1,8 @@
-using System.Text.Json;
+using System.Net.Http.Json;
 using System.Text.Json.Serialization;
 using DataPlane.Sdk.Core.Domain.Model;
+using Polly;
+using Polly.Wrap;
 using Shouldly;
 
 namespace TestRunner;
@@ -10,6 +12,10 @@ public class EndToEndTest
     private const string ConsumerHost = "http://localhost:8081";
     private const string ProviderHost = "http://localhost:8080";
     private const string ParticipantId = "dataplane-signaling-api";
+    private const string KeycloakHost = "http://localhost:8088";
+
+
+    private readonly AsyncPolicyWrap _pollyRetry;
 
     private readonly ControlPlaneSimulator _sim = new()
     {
@@ -19,18 +25,25 @@ public class EndToEndTest
         ProviderParticipant = ParticipantId // same participant id for both for the sake of simplicity
     };
 
-    // same participant id for both for the sake of simplicity
+    public EndToEndTest()
+    {
+        var timeout = Policy.TimeoutAsync(TimeSpan.FromSeconds(5));
+        var retry = Policy.Handle<Exception>().WaitAndRetryForeverAsync(_ => TimeSpan.FromMilliseconds(500));
+        _pollyRetry = Policy.WrapAsync(timeout, retry);
+    }
+
 
     [Fact]
     public async Task StreamingTest()
     {
-        var accessToken = await ObtainAccessToken("dataplane-signaling-api", "mpoTntIrYjsBqhqo0xuzqRUtCWQCWjG3");
-        accessToken.ShouldNotBeNull();
+        var accessToken = await _pollyRetry
+            .ExecuteAsync(async () => await ObtainAccessToken("dataplane-signaling-api", "mpoTntIrYjsBqhqo0xuzqRUtCWQCWjG3"));
 
-        var client = new HttpClient();
+        accessToken.ShouldNotBeNull();
 
         // prepare consumer
         var flowId = await _sim.PrepareConsumer(accessToken);
+        await Task.Delay(1000);
 
         // start provider
         var msg = await _sim.StartProvider(accessToken, flowId);
@@ -40,12 +53,14 @@ public class EndToEndTest
         msg.DataAddress.Properties.ShouldContainKey("endpointProperties");
 
         var providerDa = msg.DataAddress;
+        await Task.Delay(1000);
 
         // notify consumer started
         var response = await _sim.NotifyConsumerStarted(accessToken, providerDa, flowId);
         response.ShouldNotBeNull();
         response.DataAddress.ShouldNotBeNull();
         response.State.ShouldBeEquivalentTo(DataFlowState.Started);
+        await Task.Delay(1000);
 
         //terminate provider
         await _sim.TerminateProvider(accessToken, flowId);
@@ -54,7 +69,7 @@ public class EndToEndTest
     private async Task<string> ObtainAccessToken(string clientId, string clientSecret)
     {
         var client = new HttpClient();
-        client.BaseAddress = new Uri("http://localhost:8088");
+        client.BaseAddress = new Uri(KeycloakHost);
         var entries = new List<KeyValuePair<string, string>>
         {
             new("grant_type", "client_credentials"),
@@ -64,9 +79,7 @@ public class EndToEndTest
         };
         var result = await client.PostAsync("/realms/dataplane-signaling-api/protocol/openid-connect/token", new FormUrlEncodedContent(entries));
 
-        var at = await JsonSerializer.DeserializeAsync<AccessTokenResponse>(await result.Content.ReadAsStreamAsync());
-
-
+        var at = await result.Content.ReadFromJsonAsync<AccessTokenResponse>();
         return at != null ? at.AccessToken : throw new Exception("Failed to obtain access token");
     }
 
